e442211d8787280193eea3019e3416974876c101
Zabamund <fracgeol@gmail.com>
2017-03-12 17:14:15 +0100

    component formatting WIP, setting up links and some very basic logic

--- calendapp/src/components/CurrentUser/index.js
+++ calendapp/src/components/CurrentUser/index.js
@@ -1,4 +1,14 @@
 import React, { Component } from 'react';
+import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const styleRaisedButton = { margin: 12 };
+const stylePaper = {
+  height: 100,
+  width: 100,
+  margin: 20,
+  textAlign: 'center',
+};
 
 class CurrentUser extends Component {
 
@@ -10,10 +20,30 @@ class CurrentUser extends Component {
         }
     }
 
+    logout = (event) => {
+        event.preventDefault();
+        // clear redux state, clear local storage then go to start page and maybe display an alert ?
+        this.props.router.push('/')
+        console.log('you have logged out');
+    }
+
+    addEvent = (event) => {
+        event.preventDefault();
+        // go to event creation form
+        this.props.router.push('/events/addEvent')
+    }
+
     render() {
         return (
             <div className="CurrentUser">
-                <h3>Current User</h3>
+                <div className="App-header">
+                    <h2>Current User</h2>
+                </div>
+                <div className="headerBar">
+                    <RaisedButton onClick={this.logout} label="Logout" secondary={true} style={styleRaisedButton}/>
+                    <RaisedButton onClick={this.addEvent} label="Add Event" primary={true} style={styleRaisedButton}/>
+                    <Paper style={stylePaper} zDepth={3}>User image ?</Paper>
+                </div>
             </div>
         )
     }
