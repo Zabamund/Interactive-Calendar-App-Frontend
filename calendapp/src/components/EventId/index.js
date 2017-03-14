f8e0b0f8990858567304fdc3c49d8558608429ed
MattiasDieng <mattias.dieng@gmail.com>
2017-03-14 09:41:43 +0100

    Improved formatting on EventID component

--- calendapp/src/components/EventId/index.js
+++ calendapp/src/components/EventId/index.js
@@ -2,68 +2,72 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {List, ListItem} from 'material-ui/List';
 import Paper from 'material-ui/Paper';
-import {RaisedButton} from 'material-ui/RaisedButton';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 
 
 import './index.css';
 
-const eventsList = {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    alignItems: 'center'
-};
+const Content = {
+  display: 'flex',
+  flexWrap: 'wrap',
+  justifyContent: 'space-around',
+  alignItems: 'center',
+}
 
-const EventPicStyle = {
+const PartList = {
+  width: '30%',
   height: '60vh',
-  width: '60%',
-  margin: 20,
+  margin: '20px',
   textAlign: 'center',
-  display: 'inline-block',
-};
+}
 
-// <h3>Event: {this.props.login[this.props.params.eventId-1].eventName}</h3>
-//   this.props.login[this.props.params.eventId-1].participants.map((participant, index) => {
-//     return (
-//       <ListItem
-//         key={index}
-//         primaryText={participant.name}
-//       />
-//     )
-//   })
+const Description = {
+  width: '50%',
+  height: '60vh',
+  margin: '20px',
+  textAlign: 'center',
+}
 
 class EventId extends Component {
     render () {
         // console.log('this.props.params.eventId ', this.props.params.eventId);
         // console.log('props inside EventId', this.props);
         return(
-          <div className="Participants">
+          <div className="EventCard">
             <div className="Party-header">
               <h1>Event: {this.props.login[this.props.params.eventId].eventName}</h1>
-              <h2>Participants</h2>{/*add actual userName with this.props.currentUser.firstName*/}
             </div>
-            <div className="participantsList">
-              <List className="participantsList" style = {eventsList}>
-                {(this.props.login[this.props.params.eventId].participants)
-                    .map((participant, index) => {
-                    console.log(participant.name);
-                    return(
-                      <ListItem
-                        key={index}
-                        primaryText={participant.name}
-                      />
-                    )
-                    })
-                }
-              </List>
-              <div className="DescriptionAndImage">
-                <Paper className="eventDescription">
-                  {(this.props.login[this.props.params.eventId].description)}
-                </Paper>
-                <Paper className="eventPic" style={EventPicStyle}>
-                    EventPic here
-                </Paper>
-              </div>
+            <div className="Content" style = {Content} >
+              <Paper style = {PartList} zDepth={3}>
+                <h3>Participants</h3>
+                  <List className="participantsList">
+                    {(this.props.login[this.props.params.eventId].participants)
+                        .map((participant, index) => {
+                        console.log(participant.name);
+                        return(
+                          <ListItem
+                            key={index}
+                            primaryText={participant.name}
+                          />
+                        )
+                        })
+                    }
+                  </List>
+              </Paper>
+              <Paper style = {Description} zDepth={3}>
+                <h3>Event Info</h3>
+                <Card>
+                  <CardMedia overlay={<CardTitle title="Overlay title" />} >
+                    <img src="images/nature-600-337.jpg" />
+                  </CardMedia>
+                  <CardText>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+                    Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+                    Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+                  </CardText>
+                </Card>
+              </Paper>
             </div>
           </div>
         );
@@ -76,3 +80,16 @@ const mapStateToProps = (state) => {
 }
 
 export default connect(mapStateToProps)(EventId);
+
+
+
+
+//   <div className="DescriptionAndImage">
+//     <Paper className="eventDescription">
+//       {(this.props.login[this.props.params.eventId].description)}
+//     </Paper>
+//     <Paper className="eventPic" style={EventPicStyle}>
+//         EventPic here
+//     </Paper>
+//   </div>
+// </div>
